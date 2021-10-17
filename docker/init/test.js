4e8c0c587b0bbeccb2b3254e0ffc583be4d77110
joseuisgs <joseluisgonsan@gmail.com>
2021-10-17 18:46:05 +0200

    Init Project

--- docker/init/test.js
+++ docker/init/test.js
@@ -10,3 +10,18 @@ db.prueba.createIndex({ "address.zip": 1 }, { unique: false });
 
 db.prueba.insert({ "address": { "city": "Paris", "zip": "123" }, "name": "Mike", "phone": "1234" });
 db.prueba.insert({ "address": { "city": "Marsel", "zip": "321" }, "name": "Helga", "phone": "4321" });
+
+db = conn.getDB("blog");
+db.dropDatabase();
+
+// db = conn.getDB("blog");
+
+// Si queremos crear los datos a mano
+
+// db.category.insert({ "_id": parseInt(1), "texto": "General"});
+// db.category.insert({ "_id": parseInt(2), "texto": "Dudas"});
+// db.category.insert({ "_id": parseInt(3), "texto": "Evaluación"});
+// db.category.insert({ "_id": parseInt(4), "texto": "Pruebas" });
+
+// // Secuencia de los numeros
+// db.hibernate_sequences.insert({ "_id": "hibernate_sequence", "seq": 5 });
